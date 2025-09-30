b2ea618624e9805fcde611d3fd762d82771d35b9
diesburg <sarah.diesburg@gmail.com>
2025-09-30 15:27:53 -0500

    Updted class script for 3.2

--- scripts/3_2.sql
+++ scripts/3_2.sql
@@ -2,16 +2,18 @@ USE sakila;
 
 SHOW columns FROM film;
 
+-- How many movies are in the database?  Can we use MAX() and MIN()
+-- to figure out the answer?
+
 SELECT MAX(film_ID) FROM film;
 SELECT MIN(film_ID) FROM film;
 
 SELECT MAX(film_ID) - MIN(film_ID) FROM film;
 
-SELECT title FROM film WHERE film_id = 1;
 
-
-
--- Get a random movie to recomend based on film_id.  
+-- Management wants to automatically recommend movies
+-- to customers.  Can we write a query to get
+-- get a random movie to recomend based on film_id?
 -- (IDs range from 1 to 1000)
 
 -- Play with random
@@ -27,11 +29,6 @@ SELECT film_id, title, description
 FROM film
 WHERE film_ID = ROUND(RAND()*100,0);
 
--- Showing what's going on...
-SELECT film_id, title, description
-FROM film
-LIMIT 5;
-
 -- The fix
 SET @rand_ID = ROUND(RAND()*100,0);
 
@@ -41,7 +38,7 @@ WHERE film_ID = @rand_ID;
 
 -- Date and time functions
 
--- What is the earliest and latest rentals from sakila?
+-- What are the earliest and latest rentals from sakila?
 -- How long has the store been open?
 
 SHOW columns FROM rental;
